f7bab159ad266ad37b19c23d516303dcd67a9030
madisailors <109822907+madisailors@users.noreply.github.com>
2022-11-29 12:37:48 -0800

    draft

--- Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -10,7 +10,28 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(true && false);
+            //Console.WriteLine(true && false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true || false);
+            //Console.ReadLine();
+
+            // == operator
+            // != operator 
+            // ^ XOR operator, will generate 'true' if one is true, but not both
+
+            Console.WriteLine("Car Insurance Application");
+            Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever recieved a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you recieved?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine
+
+
 
         }
     }
