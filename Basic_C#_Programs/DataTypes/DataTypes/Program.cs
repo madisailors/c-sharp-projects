d0f47c9a0478f7f3d010e00c37febd8c4e30db10
madisailors <109822907+madisailors@users.noreply.github.com>
2022-11-28 16:18:56 -0800

    Add files via upload

---
+++ Basic_C#_Programs/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine(); 
+
+            Console.WriteLine("What is your favorite number?");
+            string yourNum = Console.ReadLine();
+            int yourNumb = Convert.ToInt32(yourNum);
+            int total = yourNumb + 5;
+            Console.WriteLine("Your favorite number 5 is: " + total);
+            Console.ReadLine();
+        }
+    }
+}
