4632ba37a224eb672f09c0ca6ccf5606ad4e4b8c
madisailors <109822907+madisailors@users.noreply.github.com>
2022-11-30 15:58:06 -0800

    updates

---
+++ Basic_C#_Programs/ArraysAnsLists/ArraysAnsLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAnsLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> intlist = new List<int>();
+            //intlist.Add(4);
+            //intlist.Add(10);
+            //intlist.Add(100);
+
+            //Console.WriteLine(intlist);
+            //Console.ReadLine();
+
+            string[] animals = { "dog", "cat", "horse", "mouse" };
+            int[] num = { 5, 10, 15, 20 };
+
+            Console.WriteLine("Please select an animal by its index (0-4): " + animals[0-3]);
+            Console.ReadLine();
+
+
+
+            //int[] numarray = new int[5];
+            //numarray[0] = 5;
+            //numarray[1] = 2;
+            //numarray[2] = 10;
+            //numarray[3] = 200;
+            //numarray[4] = 5000;
+
+            //int[] numarray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            //int[] numarray2 = { 5, 2, 10, 200, 5000 }; //easiest way 
+
+            //numarray2[5] = 650; // editing 5th number in array
+
+            //Console.WriteLine(numarray[3]);
+            //Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/Strings/Strings/Program.cs
+++ Basic_C#_Programs/Strings/Strings/Program.cs
@@ -10,16 +10,24 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            //string name = "Madi";
+            string name = "Madi";
+            string hometown = "Portland";
+            string eyeColor = "Brown";
+
+            Console.WriteLine(name + hometown + eyeColor);
+            Console.ReadLine();
             //string quote = "The woman said, \"hello\" Madi.";
             //string filename = "C:\\Users\\Madi"; // or @ before file name and no double slash
             //bool trueOrFalse = name.Contains("i");
-            //name = name.ToLower(); // also ToUpper();
-            //Console.WriteLine(name);
-            //Console.ReadLine();
+            name = name.ToLower(); // also ToUpper();
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            Console.ReadLine();
 
             StringBuilder sb = new StringBuilder();
-            sb.Append("my name is madi");  //appends an immutable string instead of creating a new one
+            sb.Append("The cow jumped over the moon;"); //appends an immutable string instead of creating a new one
+            sb.Append("The little dog laughed \nTo see such sport, ");
+            sb.Append("And the dish ran away with the spoon.");
             Console.WriteLine(sb);
             Console.ReadLine();
 
