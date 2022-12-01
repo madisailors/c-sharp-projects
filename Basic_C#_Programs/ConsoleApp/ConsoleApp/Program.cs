285cdb7657afa158327aa6ce63f77fd7a3092204
madisailors <109822907+madisailors@users.noreply.github.com>
2022-12-01 15:05:52 -0800

    edits

--- Basic_C#_Programs/ConsoleApp/ConsoleApp/Program.cs
+++ Basic_C#_Programs/ConsoleApp/ConsoleApp/Program.cs
@@ -15,11 +15,59 @@ namespace ConsoleApp
             Console.WriteLine("Please type an animal to add to the array");
             string input = Console.ReadLine();
 
-            for (int animal = 0; animal < animals.Length; animal++)
+            for (int i = 0; i < animals.Length; i++)
             {
-                animals.Add(input);
+                animals[i] = animals[i]+(input);   //take string at animals array at position i, add the user input to it, save it back to the array at position i
             }
 
+            for (int i=0; i<animals.Length; i++)
+            {
+                Console.WriteLine(animals[i]);
+                Console.ReadLine();
+            }
+
+            //assignment pt 2
+            while (true)
+            {
+                Console.WriteLine("hello world"); //infinite loop
+                break; //ends infinite loop
+            }
+
+            //assignment part 3
+            int counter = 1;
+
+            while (counter < 10)
+            {
+                Console.WriteLine("{0}", counter);
+                counter++;
+            }
+            Console.ReadLine();
+
+            int counter1 = 1;
+
+            while (counter1 <= 10)
+            {
+                Console.WriteLine("{0}", counter1);
+                counter1++;
+            }
+            Console.ReadLine();
+
+            //assignment part 4
+
+            List<string> birds = new List<string> { "Chicken", "Duck", "Crow", "Raven" };
+
+            Console.WriteLine("Please type a bird to search through the list");
+            string search = Console.ReadLine();
+
+            if (birds.Contains(search)
+                {
+                    Console.WriteLine(search);
+                }
+            else
+            {
+                Console.WriteLine("The item you entered does not match any of the birds on the list. please try again.");
+            }
+            Console.ReadLine();
         }
     }
 }
