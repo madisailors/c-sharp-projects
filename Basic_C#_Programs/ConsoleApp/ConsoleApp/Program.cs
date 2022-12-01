35c8aefd8f01d5fc6f30a3411098842cf954aa41
madisailors <109822907+madisailors@users.noreply.github.com>
2022-12-01 13:12:20 -0800

    added projects

---
+++ Basic_C#_Programs/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //part 1
+
+            string[] animals = { "dog", "cat", "horse", "mouse" };
+
+            Console.WriteLine("Please type an animal to add to the array");
+            string input = Console.ReadLine();
+        }
+    }
+}
