2623157cc19c2905765b522ba91157d6102a0c97
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-07 11:07:22 -0800

    part 5 addition

--- Basic_C#_Programs/ConsoleApp/ConsoleApp/Program.cs
+++ Basic_C#_Programs/ConsoleApp/ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -23,8 +23,8 @@ namespace ConsoleApp
             for (int i=0; i<animals.Length; i++)
             {
                 Console.WriteLine(animals[i]);
-                Console.ReadLine();
             }
+            Console.ReadLine();
 
             //assignment pt 2
             while (true)
@@ -57,20 +57,52 @@ namespace ConsoleApp
             List<string> birds = new List<string> { "Chicken", "Duck", "Crow", "Raven" };
 
             Console.WriteLine("Please type a bird to search through the list");
-            string search = Console.ReadLine();
-
-            foreach (string bird in birds)
-            {
-                if (birds.Contains(search)
+            string search = Console.ReadLine();               
+            
+                if (birds.Contains(search))
                  {
-                    Console.WriteLine(search);
+                    for (int i = 0; i < birds.Count(); i++)
+                    {
+                        if (search == birds[i])
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                    }
                 }
                 else
                 {
                     Console.WriteLine("The item you entered does not match any of the birds on the list. please try again.");
                 }
+            Console.ReadLine();
+
+
+            //assignment part 5
+
+            List<string> bugs = new List<string> { "ladybug", "ant", "fly", "beetle", "fly" };
+
+            Console.WriteLine("Please select a bug from the following list: " + bugs);
+
+            string bugSearch = Console.ReadLine();
+
+            if (bugs.Contains(bugSearch))
+            {
+                for (int i = 0; i < bugs.Count(); i++)
+                {
+                    if (search == bugs[i])
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The item you entered does not match any of the bugs on the list. please try again.");
             }
             Console.ReadLine();
+
+
+
         }
     }
 }
