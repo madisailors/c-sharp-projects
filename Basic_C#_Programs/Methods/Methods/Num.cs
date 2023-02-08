4726d3d019f29d2dd20b245f57be4165e78aac5b
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-07 18:06:39 -0800

    files added

---
+++ Basic_C#_Programs/Methods/Methods/Num.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Num
+    {
+        public static Num subtract(Num  num)
+        {
+            
+        }
+    }
+}
+//   Create a class. In that class, create three methods,
+//   each of which will take one integer parameter in
+//   and return an integer. The methods should do some
+//   math operation on the received parameter. Put this
+//   class in a separate .cs file in the application.
---
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Num num = new Num();
+            num = subtract(num);
+
+            Console.WriteLine("Please enter a number");
+            int input = Console.WriteLine(num.ToString());
+        }
+    }
+}
