62d4c6c53d6c0cdc2802f51aab05e60cf6c3d269
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-07 18:02:13 -0800

    new files

--- Basic_C#_Programs/TwentyOne/TwentyOne/card.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/card.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    class card
+   public class Card
     {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; } //public = accessable to other parts of program
+        public string Face { get; set; }
     }
 }
