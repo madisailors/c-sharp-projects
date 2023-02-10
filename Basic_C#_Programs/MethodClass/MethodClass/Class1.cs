a540bc77824285627718617f266b702924dc42df
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-09 17:07:18 -0800

    edits

---
+++ Basic_C#_Programs/MethodClass/MethodClass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Class1
+    {
+          public void Method(int num1, int num2)
+        {
+            int result = num1 + 5;
+            Console.WriteLine(result);
+            Console.WriteLine(num2);
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
+//create a class. In that class, create a void method
+//that takes two integers
+//as parameters. Have the method do a math
+//operation on the first integer
+//and display the second integer to the screen.
+//In the Main() method of the console app,
+//instantiate the class
+//Call the method in the class, passing in two numbers
+//Call the method in the class, specifying the parameters by name.
--- Basic_C#_Programs/MethodClass/MethodClass/Program.cs
+++ Basic_C#_Programs/MethodClass/MethodClass/Program.cs
@@ -10,6 +10,16 @@ namespace MethodClass
     {
         static void Main(string[] args)
         {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("Please enter a number to be added to 5");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number to be displayed");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            int number1 = class1.Method(userInput, userInput2);
+            Console.WriteLine(number1);
+
+                
         }
     }
 }
