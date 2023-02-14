dc221d2963708ff938aded391ee84bb0a78e0943
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-13 17:09:45 -0800

    updates

--- Basic_C#_Programs/TwentyOne/TwentyOne/Player.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Player.cs
@@ -8,10 +8,34 @@ namespace TwentyOne
 {
     public class Player
     {
-        public List<Card> Hand { get; set; }
+        public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+        }
+
+        private List<Card> _hand = new List<Card>();
+
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool isActivleyPlaying { get; set; }
+        public bool Stay { get; set; }
+
+        public bool Bet(int amount)
+        {
+            if (Balance - amount < 0)
+            {
+                Console.WriteLine("You do not have enough to place a bet that size");
+                return false;
+            }
+            else
+            {
+                Balance -= amount; // balance = balance - amount
+                return true;
+            }
+        }
 
         public static Game operator +(Game game, Player player) //Game and Player are what's being added together, and it's returning a game
         {   //overloading the + operator, taking Game and Player
