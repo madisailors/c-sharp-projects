a95a472a4dac5d919c2634b2678bd2656a24df17
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-13 17:22:13 -0800

    edits

--- Basic_C#_Programs/dateTime1/dateTime1/Program.cs
+++ Basic_C#_Programs/dateTime1/dateTime1/Program.cs
@@ -11,13 +11,13 @@ namespace dateTime1
         static void Main(string[] args)
         {
             Console.Write("The current date & time is : ");
-            DateTime now = DateTime.Now;
+            DateTime now = DateTime.Now;         // assigning current datetime to variable 'now'
             Console.WriteLine(now);
 
             Console.WriteLine("Please enter a number");
-            int num = Convert.ToInt32(Console.ReadLine());
+            int num = Convert.ToInt32(Console.ReadLine()); // converting user input to int and assigning it to variable
 
-            DateTime result = now.AddHours(num);
+            DateTime result = now.AddHours(num);   //using addHours method to add #of hours (num) to current datetime
             Console.WriteLine("In {0} hours, the date and time will be: ", num);
             Console.Write(result);
             Console.ReadLine();
