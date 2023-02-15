e1db1789d4af8c6e15c603537e21f008d4f504ee
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-15 15:06:43 -0800

    file transfers

---
+++ Basic_C#_Programs/constructors/constructors/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructors
+{
+    class Employee
+    {
+        public Employee(string name, int id) //assigning parameters for employee
+        {
+            Name = name;
+            Id = id;
+
+        }
+        public Employee(string name) : this(name, 0) //chaining constructors and assigning default int
+        {
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/constructors/constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Merril"); 
+
+            var employee1 = new Employee("John");
+
+
+
+
+        }
+    }
+}
+    
+    
