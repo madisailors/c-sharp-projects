3f139b8aac184515c37c8e6d02febdfcc383f1ae
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-16 10:25:14 -0800

    added techAcad project

--- Basic_C#_Programs/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ Basic_C#_Programs/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -10,8 +11,12 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            
-            return View();
+            User user = new User();
+            user.Id = 001;
+            user.FName = "Madi";
+            user.LName = "Sailors";
+            user.Age = 24;
+            return View(user);
         }
 
         public ActionResult About()
