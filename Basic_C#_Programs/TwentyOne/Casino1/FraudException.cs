b41d97a99ae4df9d3c6fb9416a568c78d0c2098c
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-16 09:27:27 -0800

    added projects

---
+++ Basic_C#_Programs/TwentyOne/Casino1/FraudException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino1
+{
+    public class FraudException : Exception
+    {
+        public FraudException() //standard constructor
+            : base() { }   //inheriting from base constructor(Exception)
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Casino;
+using Casino1;
 using Casino.TwentyOne;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Casino
 {
@@ -11,12 +13,19 @@ namespace Casino
     {
         static void Main(string[] args)
         {
-            Player newPlayer = new Player("madi");
 
             Console.WriteLine("Welcome to the Grand Hotel & Casino. Start by telling me your name. ");
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter numeric digits only, no decimals.");
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya" )
@@ -32,18 +41,59 @@ namespace Casino
                 player.isActivleyPlaying = true;
                 while (player.isActivleyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return; // void methods return no output, so this ends the program
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error has occured, please contact your system administrator");
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing, {0}", playerName);
             }
             Console.WriteLine("Feel free to look around the casino, bye for now!");
             Console.Read();
-
         }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+                                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                    TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                   (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }                                
 
         
-        }
+    }
 
 //    TwentyOneGame game = new TwentyOneGame();
 //    game.Players = new List<string>() { "bob", "sally", "john" };
