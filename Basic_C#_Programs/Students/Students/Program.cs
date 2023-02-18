cb0e0ef09b486f157966b0968d536a92fecaa6f4
madisailors <109822907+madisailors@users.noreply.github.com>
2023-02-17 17:08:11 -0800

    code first db

---
+++ Basic_C#_Programs/Students/Students/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { Id = 01, FName = "Joe", Lname = "Doe" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+            //Student student = new Student();
+
+            //student.Id = 1;
+            //student.FName = "John";
+            //student.Lname = "Doe";
+        }
+    }
+}
---
+++ Basic_C#_Programs/Students/Students/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+    }
+}
